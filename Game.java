ef105d060b02017379a380826bdb19a3b08d407b
grandbora <bora.tunca@yahoo.com>
2013-02-03 15:18:30 +0100

    export handleSecondThrow to own method

--- Game.java
+++ Game.java
@@ -3,10 +3,9 @@ package bowling;
 public class Game {
 
 	private int ball;
-	  private int firstThrow;
-	  private int secondThrow;
+	private int firstThrow;
+	private int secondThrow;
 
-	
 	private int itsScore = 0;
 	private int itsCurrentThrow = 0;
 	private int[] itsThrows = new int[21];
@@ -23,52 +22,46 @@ public class Game {
 		adjustCurrentFrame(pins);
 	}
 
-	private void adjustCurrentFrame(int pins)
-	  {
-	    if (firstThrowInFrame == true)
-	    {
-	      if( pins == 10 ) // strike
-	        itsCurrentFrame++;
-	      else
-	        firstThrowInFrame = false;
-	    }
-	    else
-	    {
-	      firstThrowInFrame=true;
-	      itsCurrentFrame++;
-	    }
-	    itsCurrentFrame = Math.min(11, itsCurrentFrame);
-	  }
+	private void adjustCurrentFrame(int pins) {
+		if (firstThrowInFrame == true) {
+			if (pins == 10) // strike
+				itsCurrentFrame++;
+			else
+				firstThrowInFrame = false;
+		} else {
+			firstThrowInFrame = true;
+			itsCurrentFrame++;
+		}
+		itsCurrentFrame = Math.min(11, itsCurrentFrame);
+	}
 
-	public int scoreForFrame(int theFrame)
-	  {
-	    ball = 0;
-	    int score=0;
-	    for (int currentFrame = 0; 
-	         currentFrame < theFrame; 
-	         currentFrame++)
-	    {
-	      firstThrow = itsThrows[ball++];
-	      if (firstThrow == 10)
-	      {
-	        score += 10 + itsThrows[ball] + itsThrows[ball+1];
-	      }
-	      else
-	      {
-	        secondThrow = itsThrows[ball++];
+	public int scoreForFrame(int theFrame) {
+		ball = 0;
+		int score = 0;
+		for (int currentFrame = 0; currentFrame < theFrame; currentFrame++) {
+			firstThrow = itsThrows[ball++];
+			if (firstThrow == 10) {
+				score += 10 + itsThrows[ball] + itsThrows[ball + 1];
+			} else {
+				score += handleSecondThrow();
+			}
+		}
 
-	        int frameScore = firstThrow + secondThrow;
-	        // spare needs next frames first throw
-	        if ( frameScore == 10 )
-	          score += frameScore + itsThrows[ball];
-	        else
-	          score += frameScore;
-	      } 
-	    }
+		return score;
+	}
 
-	    return score;
-	  }
+	private int handleSecondThrow() {
+		int score = 0;
+		secondThrow = itsThrows[ball++];
 
+		int frameScore = firstThrow + secondThrow;
+		// spare needs next frames first throw
+		if (frameScore == 10)
+			score += frameScore + itsThrows[ball];
+		else
+			score += frameScore;
+		return score;
+	}
 
 	public int getCurrentFrame() {
 		return itsCurrentFrame;
